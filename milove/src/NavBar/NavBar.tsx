31895e5b58b7f14a56029fae5af0addd8a716695
syi0 <syi0.v006@gmail.com>
2024-10-31 10:47:10 +0100

    G

--- milove/src/NavBar/NavBar.tsx
+++ milove/src/NavBar/NavBar.tsx
@@ -4,9 +4,10 @@ export default function NavBar() {
     return(
         <nav>
             <ul>
-                <li><a href="">Home <i className="fa-solid fa-house"></i></a></li>
+                <li><a href="/">Home <i className="fa-solid fa-house"></i></a></li>
+                <li><a href="/social">Find your miLove! <i className="fa-regular fa-heart"></i></a></li>
+                <li><a href="/login" className='active'>Login <i className="fa-solid fa-user"></i></a></li>
             </ul>
-            <button>w</button>
         </nav>
     );
 }
