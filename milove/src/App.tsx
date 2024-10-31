f366f3777b08545e3c8c9a67e401332f3b9ff4d5
syi0 <syi0.v006@gmail.com>
2024-10-31 10:47:10 +0100

    G

--- milove/src/App.tsx
+++ milove/src/App.tsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import NavBar from './NavBar/NavBar'
+import Body from './MainBody/MainBody'
 
 function App() {
   return (
     <>
       <NavBar />
+      <Body />
     </>
   )
 }
--- milove/src/MainBody/MainBody.tsx
+++ milove/src/MainBody/MainBody.tsx
@@ -2,6 +2,11 @@ import './MainBody.css'
 
 export default function MainBody() {
     return(
-        <div></div>
+        <div>
+            <img src="" alt="LOGO" /> <br />
+            <button>Join to MILOVE!</button> <br />
+            <h1>Why us?</h1>
+            <p>Lorem funiusbgiuyvesrbvubaiegi</p>
+        </div>
     );
 }
