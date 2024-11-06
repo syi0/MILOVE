949e9c305eac1b432efa614f7ec0fdb9fa78773f
swk563 <stefcio.wujo@gmail.com>
2024-11-06 12:04:42 +0100

    added footer

--- milove/src/App.tsx
+++ milove/src/App.tsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import './App.css'
 import NavBar from './NavBar/NavBar'
 import Body from './MainBody/MainBody'
+import Footer from './Footer/Footer'
 
 function App() {
   return (
     <>
       <NavBar />
       <Body />
+      <Footer />
     </>
   )
 }
