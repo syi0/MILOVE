efe1bc56c49481adc2f262d2bacad862ddd6d73f
syi0 <syi0.v006@gmail.com>
2024-11-06 10:05:14 +0100

    main tsx /

--- milove/src/main.tsx
+++ milove/src/main.tsx
@@ -2,6 +2,28 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import reportWebVitals from './reportWebVitals'
+import Login from './Login/Login.tsx'
+import Register from './Register/Register.tsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path: "/login",
+    element: <Login/>,
+  },
+  {
+    path: "/register",
+    element: <Register/>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
