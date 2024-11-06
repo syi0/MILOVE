0fa42c3d5969519031dd608ba5420bc5c9d621d7
syi0 <syi0.v006@gmail.com>
2024-11-06 11:15:28 +0100

    added login and register

--- milove/src/MainBody/MainBody.tsx
+++ milove/src/MainBody/MainBody.tsx
@@ -5,7 +5,7 @@ export default function MainBody() {
     return(
         <div>
             <img src={Logo} alt="LOGO" /> <br />
-            <button>Join to MILOVE!</button> <br />
+            <a href="/register"><button>Join to MILOVE!</button></a> <br />
             <h1>Why us?</h1>
             <p>Lorem funiusbgiuyvesrbvubaiegi</p>
         </div>
--- milove/src/Register/Register.tsx
+++ milove/src/Register/Register.tsx
@@ -2,6 +2,17 @@ import './Register.css'
 
 export default function Register() {
     return(
-        <div></div>
+        <div>
+            <form action="" className='Register'>
+                <label htmlFor="username">Username: </label><input type="text" name="username" id="username" placeholder='Name'/> <br /> <br />
+                <label htmlFor="email">Email: </label><input type="email" name="email" id="email" placeholder='uwu@gmail.com'/><br /> <br />
+                <label htmlFor="password">Password: </label><input type="password" name="password" id="password" placeholder='Password123'/> <br /> <br />
+                <label htmlFor="NaS">Name and Surname: </label><input type="text" name="NaS" id="NaS" placeholder='Miłosz Ratkiewicz'/><br /> <br />
+                <label htmlFor="age">Your age: </label><input type="number" name="age" id="age" placeholder='min. 18 yo'/><br /> <br />
+                <button>Register</button> <br />
+                <p>You have account? <a href="/login"><b>Login!</b></a></p> <br />
+                <a href="/"><b><i className="fa-solid fa-arrow-left"></i> Home</b></a>
+            </form>
+        </div>
     );
 }
