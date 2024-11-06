6fa638e77e20fe62a548b56d8040743719ecb9ce
syi0 <syi0.v006@gmail.com>
2024-11-06 10:00:29 +0100

    Added Logo

--- milove/src/MainBody/MainBody.tsx
+++ milove/src/MainBody/MainBody.tsx
@@ -1,9 +1,10 @@
 import './MainBody.css'
+import Logo from '../!images/LOGO.png'
 
 export default function MainBody() {
     return(
         <div>
-            <img src="" alt="LOGO" /> <br />
+            <img src={Logo} alt="LOGO" /> <br />
             <button>Join to MILOVE!</button> <br />
             <h1>Why us?</h1>
             <p>Lorem funiusbgiuyvesrbvubaiegi</p>
