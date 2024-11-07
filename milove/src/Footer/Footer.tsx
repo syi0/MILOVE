52ff59c9be0136f0951b2a86149337acdf1daae9
swk563 <stefcio.wujo@gmail.com>
2024-11-07 13:10:06 +0100

    footer update

--- milove/src/Footer/Footer.tsx
+++ milove/src/Footer/Footer.tsx
@@ -5,9 +5,9 @@ export default function () {
         <footer>
 
             <a className='footerTile'>&copy;MILOVE 2024</a>
-            <a href="#" className='footerTile'><i className="fa-brands fa-instagram"></i> Instagram</a>
-            <a href="#" className='footerTile'><i className="fa-brands fa-square-facebook"></i> Facebook</a>
-            <a href="#" className='footerTile'><i className="fa-regular fa-envelope"></i> Contact us</a>
+            <a href="#" className='footerTile'><i className="fa-brands fa-instagram"></i></a>
+            <a href="#" className='footerTile'><i className="fa-brands fa-square-facebook"></i></a>
+            <a href="#" className='footerTile'> Contact us</a>
 
         </footer>
     );
