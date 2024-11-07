35e76b36008eead1878a1345b7425e214573bfb0
syi0 <syi0.v006@gmail.com>
2024-11-07 13:58:01 +0100

    few actualizations and bug repairs

--- milove/src/Footer/Footer.tsx
+++ milove/src/Footer/Footer.tsx
@@ -3,12 +3,12 @@ import './Footer.css'
 export default function () {
     return(
         <footer>
-
-            <a className='footerTile'>&copy;MILOVE 2024</a>
-            <a href="#" className='footerTile'><i className="fa-brands fa-instagram"></i></a>
-            <a href="#" className='footerTile'><i className="fa-brands fa-square-facebook"></i></a>
-            <a href="#" className='footerTile'> Contact us</a>
-
+            <div className='links'>
+                <a className='footerTile' href='/'>&copy;MILOVE 2024</a>
+                <a href="#" className='footerTile'><i className="fa-brands fa-instagram"></i></a>
+                <a href="#" className='footerTile'><i className="fa-brands fa-square-facebook"></i></a>
+                <a href="mailto:support@milove.pl" className='footerTile'> Contact us</a>
+            </div>
         </footer>
     );
 }
--- milove/src/MainBody/MainBody.tsx
+++ milove/src/MainBody/MainBody.tsx
@@ -1,5 +1,6 @@
 import './MainBody.css'
 import Logo from '../!images/LOGO.png'
+import Footer from '../Footer/Footer';
 
 export default function MainBody() {
     return(
@@ -7,7 +8,9 @@ export default function MainBody() {
             <img src={Logo} alt="LOGO" /> <br />
             <a href="/register"><button>Join to MILOVE!</button></a> <br />
             <h1>Why us?</h1>
-            <p>Lorem funiusbgiuyvesrbvubaiegi</p>
+            <p>MILOVE is designed to help you find real connections with ease.<br/> Our smart matching system pairs you with people who share your interests and values, making it simpler to meet like-minded singles.<br/> With a user-friendly platform and a diverse community, MILOVE makes online dating enjoyable and effective.</p>
+            <Footer />
         </div>
+        
     );
 }
