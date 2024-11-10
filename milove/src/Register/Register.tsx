333734699efedb055453759cc9bafc9fce5936ed
syi0 <syi0.v006@gmail.com>
2024-11-10 22:38:28 +0100

    Added some functionality like authentication etc. + firebase working hell yeah

--- milove/src/Register/Register.tsx
+++ milove/src/Register/Register.tsx
@@ -1,17 +1,40 @@
 import './Register.css'
 import Logo from '../!images/LOGO.png'
+import React, { useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
+import { auth } from "../firebase";
 
 export default function Register() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [username, setUsername] = useState("");
+
+    const handleSignUp = (event) => {
+        event.preventDefault();
+        createUserWithEmailAndPassword(auth, email, password)
+          .then((authUser) => {
+            signInWithEmailAndPassword(auth, email, password).then(
+              updateProfile(auth.currentUser, {
+                displayName: username,
+              })
+            );
+          })
+          .catch((err) => {
+            alert(err);
+        });
+      };
     return (
         <div>
             <form action="" className='Register'>
                 <img src={Logo} alt="LOGO" /> <br />
-                <label htmlFor="username">Username: </label><br /><input type="text" name="username" id="username" placeholder='Nickname' /> <br /> <br />
-                <label htmlFor="email">Email: </label><br /><input type="email" name="email" id="email" placeholder='name@domain.com' /><br /> <br />
-                <label htmlFor="password">Password: </label><br /><input type="password" name="password" id="password" placeholder='Password' /> <br /> <br />
-                <label htmlFor="NaS">Name and Surname: </label><br /><input type="text" name="NaS" id="NaS" placeholder='Miłosz Ratkiewicz' /><br /> <br />
-                <label htmlFor="age">Your age: </label><br /><input type="number" name="age" id="age" placeholder='min. 18 yo' /><br /> <br />
-                <button>Register</button> <br />
+                <label htmlFor="username">Username: </label><br /><input type="text" name="username" id="username" placeholder='Nickname' value={username} onChange={(e) => setUsername(e.target.value)}/> <br /> <br />
+                <label htmlFor="email">Email: </label><br /><input type="email" name="email" id="email" placeholder='name@domain.com' value={email} onChange={(e) => setEmail(e.target.value)} /><br /> <br />
+                <label htmlFor="password">Password: </label><br /><input type="password" name="password" id="password" placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)}/> <br /> <br />
+                <button onClick={handleSignUp}>Sign up</button> <br />
                 <p>You have account? <a href="/login"><b>Login!</b></a></p> <br />
                 <a href="/"><b><i className="fa-solid fa-arrow-left"></i> Home</b></a>
             </form>
--- milove/src/main.tsx
+++ milove/src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import reportWebVitals from './reportWebVitals.tsx'
 import Login from './Login/Login.tsx'
 import Register from './Register/Register.tsx'
 import Social from './MainSocial/Homepage.tsx'
@@ -10,6 +9,8 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import { Provider } from 'react-redux'
+import {store} from "./app/store"
 
 const router = createBrowserRouter([
   {
@@ -31,9 +32,9 @@ const router = createBrowserRouter([
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <RouterProvider router={router} />
-  </React.StrictMode>,
+  </Provider>,
 )
 
 // Use contextBridge
