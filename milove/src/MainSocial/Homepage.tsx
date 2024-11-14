0b15108ff5d485514ee748cbd701d6bf24f7995b
mm554 <murawskim994@gmail.com>
2024-11-14 13:41:26 +0100

    logout

--- milove/src/MainSocial/Homepage.tsx
+++ milove/src/MainSocial/Homepage.tsx
@@ -2,10 +2,35 @@ import { useSelector } from 'react-redux';
 import './Homepage.css'
 import SideNav from './SideNav';
 import TimeLine from './TimeLine';
-
+import { browserSessionPersistence, onAuthStateChanged, setPersistence, User,} from "firebase/auth";
+import { auth } from "../firebase";
+import { useEffect, useState } from 'react';
 export default function Homepage() {
-    console.log(useSelector((state) => state));
-    
+    const [user, setUser] = useState(null);
+ useEffect(() => {
+    async function test() {
+        
+   
+    await onAuthStateChanged(auth, (userr) => {
+     
+        if (userr != null) {
+          // User is signed in, see docs for a list of available properties
+          // https://firebase.google.com/docs/reference/js/auth.user
+           setUser(userr);
+        
+          // ...
+        } else {
+          // User is signed out
+          // ...
+        }
+      });
+      
+    }
+    test();
+    console.log(user);
+   
+});
+
     return(
         <div className='homepage'>
             <div className='homepage_nav'><SideNav/></div>
