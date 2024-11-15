066f067da70f96f58c2e85542678a25848ab2288
syi0 <syi0.v006@gmail.com>
2024-11-15 19:36:50 +0100

    Added base UML graph

--- milove/src/GraphUML/Graph.tsx
+++ milove/src/GraphUML/Graph.tsx
@@ -6,28 +6,41 @@ import VisGraph, {
 export default function GraphView() {
   const graph: GraphData = {
     nodes: [
-      { id: 1, label: 'Node 1', title: 'node 1 tootip text' },
-      { id: 2, label: 'Node 2', title: 'node 2 tootip text' },
-      { id: 3, label: 'Node 3', title: 'node 3 tootip text' },
-      { id: 4, label: 'Node 4', title: 'node 4 tootip text' },
-      { id: 5, label: 'Node 5', title: 'node 5 tootip text' },
+      { id: 2, label: 'Auth', title: 'Authorization page' },
+      { id: 1, label: 'Welcome page', title: 'Welcome page with some elements on it' },
+      { id: 3, label: 'Register', title: 'register page with form for SignUp' },
+      { id: 4, label: 'Login', title: 'login page with form for SignIn' },
+      { id: 5, label: 'Email', title: 'EmailInput = email' },
+      { id: 6, label: 'Password', title: 'PasswordInput = password' },
+      { id: 7, label: 'Username', title: 'UsernameInput = text'},
+      { id: 8, label: 'SocialApp', title: 'SocialApp is main body of social application'}
     ],
     edges: [
       { from: 1, to: 2 },
-      { from: 1, to: 3 },
+      { from: 2, to: 3 },
       { from: 2, to: 4 },
-      { from: 2, to: 5 },
+      {from: 7, to: 3},
+      { from: 5, to: 3 },
+      { from: 6, to: 3},
+      {from: 5, to: 4},
+      { from: 6, to: 4 },
+      { from: 2, to: 8},
+      { from: 1, to: 8},
+      { from: 8, to: 1},
+      { from: 8, to: 2},
+      
+      
     ],
   };
 
   const options: Options = {
     layout: {
-      hierarchical: true,
+      hierarchical: false,
     },
     edges: {
       color: '#000000',
     },
-    height: '500px',
+    height: '1000px',
   };
 
   const events: GraphEvents = {
@@ -44,7 +57,7 @@ export default function GraphView() {
       ref={(network: Network) => {
         //  if you want access to vis.js network api you can set the state in a parent component using this property
         console.log(network);
-      }}
+      }} className='graph'
     />
   );
 }
