9f02bbeaadfa35d0612e41d3ffead5220819b197
mm554 <murawskim994@gmail.com>
2024-11-21 12:26:37 +0100

    fixed

--- milove/src/MainSocial/Homepage.tsx
+++ milove/src/MainSocial/Homepage.tsx
@@ -3,7 +3,6 @@ import './Homepage.css'
 import SideNav from './SideNav';
 import TimeLine from './TimeLine';
 import { browserSessionPersistence, onAuthStateChanged, setPersistence, User,} from "firebase/auth";
-<<<<<<< HEAD
 import { auth, db } from "../firebase";
 import { useEffect, useState } from 'react';
 import { addDoc, collection, doc, getDocs } from 'firebase/firestore';
@@ -36,12 +35,6 @@ export default function Homepage() {
           }
         }
           ,[userdata,user]);
-=======
-import { auth } from "../firebase";
-import { useEffect, useState } from 'react';
-export default function Homepage() {
-    const [user, setUser] = useState(null);
->>>>>>> 6dcffcf22863cdd7c99a1161bc3abfee3f1b12f3
  useEffect(() => {
     async function test() {
         
@@ -62,11 +55,7 @@ export default function Homepage() {
       
     }
     test();
-<<<<<<< HEAD
    
-=======
-    console.log(user);
->>>>>>> 6dcffcf22863cdd7c99a1161bc3abfee3f1b12f3
    
 });
 
