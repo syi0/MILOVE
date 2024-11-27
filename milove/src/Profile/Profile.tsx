2e84f77897f03c4b10596eb21644dbb9659032ba
mm554 <murawskim994@gmail.com>
2024-11-27 12:15:11 +0100

    image work

--- milove/src/Profile/Profile.tsx
+++ milove/src/Profile/Profile.tsx
@@ -4,6 +4,7 @@ import { onAuthStateChanged } from 'firebase/auth';
 import { auth, db } from '../firebase';
 import { collection, doc, getDocs, setDoc } from 'firebase/firestore';
 import { getStorage, ref, uploadBytes } from "firebase/storage";
+import axios from "axios";
 export default function Profile() {
     const [user,setUser]=useState(null);
     const [userdata,setUserdata] = useState(null);
@@ -21,6 +22,48 @@ export default function Profile() {
             console.log('Uploaded a blob or file!');
           });
     }*/
+          function convertBase64ToFile(image) {
+            const byteString = atob(image.split(',')[1]);
+            const ab = new ArrayBuffer(byteString.length);
+            const ia = new Uint8Array(ab);
+            for (let i = 0; i < byteString.length; i += 1) {
+              ia[i] = byteString.charCodeAt(i);
+            }
+            const newBlob = new Blob([ab], {
+              type: 'image/jpeg',
+            });
+            return newBlob;
+          }
+        function  uplBase64(file) {
+            return new Promise(resolve => {
+              let fileInfo;
+              let baseURL = "";
+              // Make new FileReader
+              let reader = new FileReader();
+        
+              // Convert the file to base64 text
+              reader.readAsDataURL(file);
+        
+              // on reader load somthing...
+              reader.onload = () => {
+                // Make a fileInfo Object
+                baseURL = reader.result;
+                console.log(baseURL);
+                //resolve(baseURL);
+                setImg(URL.createObjectURL(new File([convertBase64ToFile(baseURL)],"pfp")));
+                setDoc(doc(db, "userdata","imgtest"), {   
+                  img: baseURL
+                });
+              }
+          });
+        }
+        
+          
+   function imgchng(e) {
+    console.log(e.target.files[0]);
+   
+   uplBase64(e.target.files[0]);
+   }
    function ageChange(e){
 
     if(e.code=="Enter") {
@@ -87,7 +130,7 @@ export default function Profile() {
     return(
         <div className='profile'>
             <div className="profile_data">
-                <div className='profile_icon'>{user.photoURL==null ? "brak zdjecia" : <img src={user.photoURL}></img>} <input type="file" /></div>
+                <div className='profile_icon'>{img==null ? "brak zdjecia" : <img src={img}></img>} <input onChange={imgchng}type="file" /></div>
                 <div className="profile_info">{user.displayName} + {age==null ? <input onKeyDownCapture={ageChange}></input> : age}. If age is null, you need to click button to enter that shit</div>
                 <div className="profile_description">{desc==null ? "brak opisu" : desc}Description of the profile, user must say anything about yourself</div>
                 <div className="profile_message_button">Button that user must click to chat with that person</div>
