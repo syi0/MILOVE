74a15e19ca8735cc78f0da24218f40b837aa5e28
mm554 <murawskim994@gmail.com>
2024-12-11 12:11:57 +0100

    searc

--- milove/src/ProfileEditor/ProfileEditor.tsx
+++ milove/src/ProfileEditor/ProfileEditor.tsx
@@ -17,7 +17,7 @@ export default function ProfileEditor(props) {
         <label htmlFor="age">Age: </label><input value={props.age} type="number" name="age" id="age" /><br />
         <label htmlFor="name_and_surname">Name and Surname: </label><input type="text" name="name_and_surname" id="name_and_surname" /> <br />
         <label htmlFor="descritption">Description: </label><textarea value={props.desc} name="description" id="description"></textarea><br />
-        <button type='button'>Save</button>
+        <button onClick={save} type='button'>Save</button>
     </div>
   );
 }
